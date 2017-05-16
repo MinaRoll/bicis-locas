f98caecc0a64da1081517ccf4eb53700a7146b64
romina <romina.torres.b@gmail.com>
2017-05-16 01:31:21 -0300

    ejercicio terminado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,66 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+
+	function name(){
+		var idNombre = document.getElementById("name").value;
+		if (!(/^[A-Z][a-z]{3,19}$/).test(idNombre)){
+		var spanName = document.createElement("span");
+		var spanText = document.createTextNode("Nombre inválido. Ingrese nuevamente con inicial en mayúscula.");
+		var spanFather = document.getElementsByClassName("name-container input-box")[0];
+		spanName.appendChild(spanText);
+		spanFather.appendChild(spanName);
+		}
+	}
+	name();
+
+	function lastName(){
+		var idApellido = document.getElementById("lastname").value;
+		if (!(/^[A-Z][a-z]{3,19}$/).test(idApellido)){
+			var spanLastName = document.createElement("span");
+			var spanText = document.createTextNode("Apellido inválido. Ingrese nuevamente con inicial en mayúscula.");
+			var spanFather = document.getElementsByClassName("lastname-container input-box")[0];
+			spanLastName.appendChild(spanText);
+			spanFather.appendChild(spanLastName);
+		}
+	}
+	lastName();
+
+	function email(){
+		var idCorreo = document.getElementById("input-email").value;
+		if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(idCorreo)){
+			var spanCorreo = document.createElement("span");
+			var spanText = document.createTextNode("Correo inválido. Ingrese nuevamente.");
+			var spanFather = document.getElementsByClassName("email-container input-box")[0];
+			spanCorreo.appendChild(spanText);
+			spanFather.appendChild(spanCorreo);
+		}
+	}
+	email();
+
+	function contrasena(){
+		var idContrasena = document.getElementById("input-password").value;
+		if (idContrasena=="123456" || idContrasena=="098765" || !(/^(?=.*\d).{6,}$/).test(idContrasena)){
+			var spanContrasena = document.createElement("span");
+			var spanText = document.createTextNode("Contraseña inválida. Ingrese nuevamente.");
+			var spanFather = document.getElementsByClassName("form-group input-box")[0];
+			spanContrasena.appendChild(spanText);
+			spanFather.appendChild(spanContrasena);
+		}
+	}
+	contrasena();
+
+	function selection(){
+		var election = document.getElementsByTagName("select");
+		var large = election.length;
+		for (i=0 ; i<large ; i++){
+			if (election[i].value == 0){
+				var spanEleccion = document.createElement("span");
+				var spanText = document.createTextNode("Debe escoger una opción");
+				var spanFather = document.getElementsByClassName("form-group input-box")[1];
+				spanEleccion.appendChild(spanText);
+				spanFather.appendChild(spanEleccion);
+			}
+		}
+	}
+	selection();
+
 }
